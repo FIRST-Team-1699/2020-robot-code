0d6ddb8ba2b6888c840036fa44fdaae647495062
Jakob Misbach <jakobmisbach8@gmail.com>
2019-11-14 22:35:58 -0500

    Added test that should cause CI to fail

--- robot-code/src/main/tst/ExampleTest.java
+++ robot-code/src/main/tst/ExampleTest.java
@@ -1,2 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ExampleTest {
+
+    @Test
+    public void testTest(){
+        assertEquals(1, 2);
+    }
 }
