0596dea53ecd01e48f77e68753408a62f0e77074
squirlemaster42 <jakobmisbach8@gmail.com>
2019-12-14 14:47:29 -0500

    Started TalonSRX Utils

--- robot-code/src/main/java/team1699/utils/controllers/BetterSpeedController.java
+++ robot-code/src/main/java/team1699/utils/controllers/BetterSpeedController.java
@@ -1,8 +1,12 @@
 package team1699.utils.controllers;
 
-public class BetterSpeedController{
+public abstract class BetterSpeedController{
 
+	//TODO Populate
 	public BetterSpeedController(){
 
 	}
+
+	public abstract void set(final double percent);
+	public abstract double get();
 }
